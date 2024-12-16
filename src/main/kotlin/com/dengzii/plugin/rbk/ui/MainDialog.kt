8948196e7b5ee1e4034e82983d1433858395a775
duoshilin <duoshilin@moqipobing.com>
2024-12-16 13:24:26 +0800

    feat: UI 优化

--- src/main/kotlin/com/dengzii/plugin/rbk/ui/MainDialog.kt
+++ src/main/kotlin/com/dengzii/plugin/rbk/ui/MainDialog.kt
@@ -5,9 +5,13 @@ import com.dengzii.plugin.rbk.Config.insertCallBindViewMethodAfterCallMethod
 import com.dengzii.plugin.rbk.Config.insertCallBindViewToFirstLine
 import com.dengzii.plugin.rbk.Config.methodNameBindView
 import com.dengzii.plugin.rbk.Config.priorityReplaceButterKnifeBind
+import com.intellij.ui.util.minimumWidth
+import com.intellij.ui.util.preferredHeight
+import com.intellij.ui.util.preferredWidth
 import java.awt.Dimension
 import java.awt.Toolkit
 import java.awt.event.ActionEvent
+import java.awt.event.ItemEvent
 import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
@@ -18,6 +22,7 @@ class MainDialog(private val callback: Callback) : JDialog() {
     var insertToMethodTextField: JTextField? = null
     var undoWhenRemoveFailedCheckBox: JCheckBox? = null
     var bindViewMethodNameField: JTextField? = null
+    var insertToMethodLabel: JLabel? = null
     var insertAfterTextField: JTextField? = null
     var OKButton: JButton? = null
     var contentPanel: JPanel? = null
@@ -33,13 +38,24 @@ class MainDialog(private val callback: Callback) : JDialog() {
                 ok()
             }
         })
+
+        priorityToSearchAndCheckBox!!.addItemListener { e ->
+            val isSelect = e.stateChange == ItemEvent.SELECTED
+            priorityReplaceButterKnifeBind = isSelect
+
+            insertToMethodLabel?.isVisible = !isSelect
+            insertToMethodTextField?.isVisible = !isSelect
+
+            contentPanel!!.minimumWidth = if (isSelect) 300 else 650
+        }
+
         contentPanel!!.registerKeyboardAction(
-            { e: ActionEvent? -> dispose() },
+            { _: ActionEvent? -> dispose() },
             KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
             JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
         )
         contentPanel!!.registerKeyboardAction(
-            { e: ActionEvent? -> ok() },
+            { _: ActionEvent? -> ok() },
             KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
             JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
         )
